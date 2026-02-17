cf363629e762511de5c286ecab11fc9eeaae8d2b
sabbir053 <sabbirhossain0900@gmail.com>
2026-02-18 04:31:49 +0600

    Fully Compleate login page with functionality's

---
+++ script/login.js
@@ -0,0 +1,16 @@
+// console.log("test login js file")
+
+document.getElementById('login-btn').addEventListener('click', () => {
+    const numberInput = document.getElementById('input-number');
+    const inputNumberValue = numberInput.value;
+
+    const pinInput = document.getElementById('input-pin');
+    const inputPinValue = pinInput.value;
+
+    if (inputNumberValue == '01734521053' && inputPinValue == '5479') {
+        window.location.assign('/home.html');
+    } else {
+        alert("Wrong | Number or Password -- Please Try a correct value.");
+        return;
+    }
+})
